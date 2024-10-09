30d622787dff1dfadd518192cb9bad5f87274010
Amvlc <168634246+Amvlc@users.noreply.github.com>
2024-10-09 13:30:48 +0800

    Add files via upload

--- blogicum/blog/query_utils.py
+++ blogicum/blog/query_utils.py
@@ -7,7 +7,7 @@ from .models import Post
 def get_post_queryset(
     manager=Post.objects, filter_published=True, annotate_comments=True
 ):
-    queryset = manager.select_related('author', 'category', 'location')
+    queryset = manager.select_related()
     if filter_published:
         queryset = queryset.filter(
             is_published=True,
